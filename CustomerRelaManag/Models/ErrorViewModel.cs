82a1fd9bb6610827a39a5490aa492513c928a94c
Shanti Rajput <santirajput0@gmail.com>
2023-04-15 06:28:15 +0530

    Add project files.

---
+++ CustomerRelaManag/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerRelaManag.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
